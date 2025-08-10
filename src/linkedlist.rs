18ce49915f38a55788750334a3879f88694adae3
Alena Weiley <alena@weiley.org>
2025-08-11 01:22:04 +1000

    change some method names and implement iterator

--- src/linkedlist.rs
+++ src/linkedlist.rs
@@ -1,10 +1,8 @@
-pub enum List<T> {
+enum List<T> {
     Cons(T, Box<List<T>>),
     Nil,
 }
 
-// so we don't have to write `std::mem::replace(my_list, List::Nil)`
-// and instead can write `std::mem::take(my_list)`
 impl<T> Default for List<T> {
     fn default() -> List<T> {
         List::Nil
@@ -17,27 +15,12 @@ impl<T> List<T> {
         List::Nil
     }
 
-    fn get_root(&self) -> Option<&T> {
-        self.get(0)
-    }
-
-    fn get(&self, index: usize) -> Option<&T> {
+    fn retrieve(&self, index: usize) -> Option<&T> {
         match self {
             List::Nil => None,
             List::Cons(x, _) if index == 0 => Some(x),
-            List::Cons(_, tail) => tail.get(index - 1),
+            List::Cons(_, tail) => tail.retrieve(index - 1),
         }
-    }
-
-    fn len(&self) -> usize {
-        match self {
-            List::Nil => 0,
-            List::Cons(_, tail) => 1 + tail.len(),
-        }
-    }
-
-    fn prepend(&mut self, x: T) {
-        *self = List::Cons(x, Box::new(std::mem::take(self)));
     }
 
     fn append(&mut self, x: T) {
@@ -52,115 +35,69 @@ impl<T> List<T> {
     fn remove(&mut self, index: usize) -> Option<T> {
         match self {
             List::Nil => None,
-            List::Cons(_, _) if index == 0 => if let List::Cons(x, tail) = std::mem::take(self) {
-                *self = *tail;
-                Some(x)
-            } else {
-                None
+            List::Cons(_, _) if index == 0 => {
+                if let List::Cons(x, tail) = std::mem::take(self) {
+                    *self = *tail;
+                    Some(x)
+                } else { None }
             },
             List::Cons(_, tail) => tail.remove(index - 1),
         }
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    fn prepend(&mut self, x: T) {
+        *self = List::Cons(x, Box::new(std::mem::take(self)));
+    }
 
-    #[test]
-    fn test_construct_list() {
-        let list: List<i32> = List::new();
-        match list {
-            List::Nil => assert!(true),
-            _ => panic!("expected empty list"),
+    fn size(&self) -> usize {
+        match self {
+            List::Nil => 0,
+            List::Cons(_, tail) => 1 + tail.size(),
         }
     }
+}
 
-    #[test]
-    fn test_root_is_none() {
-        let list: List<i32> = List::new();
-        let root_obj = list.get_root();
-        assert!(root_obj.is_none());
+/* owned iteration (for x in xs) */
+
+impl<T> Iterator for List<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match std::mem::take(self) {
+            List::Cons(x, tail) => {
+                *self = *tail;
+                Some(x)
+            },
+            List::Nil => None,
+        }
     }
+}
 
-    #[test]
-    fn test_append_to_root() {
-        let mut list: List<i32> = List::new();
-        list.append(1);
-        let root_obj = list.get_root();
-        assert!(root_obj.is_some());
+/* borrowed iteration (for x in &xs) */
+
+struct ListIter<'a, T> {
+    current: Option<&'a List<T>>,
+}
+
+impl<'a, T> Iterator for ListIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.current {
+            Some(List::Cons(x, tail)) => {
+                self.current = Some(tail);
+                Some(x)
+            },
+            _ => None,
+        }
     }
+}
 
-    #[test]
-    fn test_append_and_get_0() {
-        let mut list: List<i32> = List::new();
-        list.append(1);
-        let root_obj = list.get_root();
-        assert!(root_obj.is_some());
-        let obj_retrieved = list.get(0);
-        assert_eq!(obj_retrieved, Some(&1));
-    }
+impl<'a, T> IntoIterator for &'a List<T> {
+    type Item = &'a T;
+    type IntoIter = ListIter<'a, T>;
 
-    #[test]
-    fn test_multiple_append_and_get_1() {
-        let mut list: List<i32> = List::new();
-        list.append(10);
-        list.append(20);
-        let root_obj = list.get_root();
-        assert!(root_obj.is_some());
-        let obj_retrieved = list.get(1);
-        assert_eq!(obj_retrieved, Some(&20));
-    }
-
-    #[test]
-    fn test_multiple_append_and_get_3() {
-        let mut list: List<i32> = List::new();
-        list.append(10);
-        list.append(20);
-        list.append(30);
-        list.append(40);
-        list.append(50);
-        let root_obj = list.get_root();
-        assert!(root_obj.is_some());
-        let obj_retrieved = list.get(3);
-        assert_eq!(obj_retrieved, Some(&40));
-    }
-
-    #[test]
-    fn test_append_once_and_remove_0() {
-        let mut list: List<i32> = List::new();
-        list.append(10);
-        let obj = list.remove(0);
-        assert_eq!(obj, Some(10));
-    }
-
-    #[test]
-    fn test_multiple_append_and_remove_1() {
-        let mut list: List<i32> = List::new();
-        list.append(10);
-        list.append(20);
-        list.append(30);
-        list.append(40);
-        list.append(50);
-        let obj = list.remove(1);
-        assert_eq!(obj, Some(20));
-    }
-
-    #[test]
-    fn test_multiple_append_and_remove_3() {
-        let mut list: List<i32> = List::new();
-        list.append(10);
-        list.append(20);
-        list.append(30);
-        list.append(40);
-        list.append(50);
-        let obj = list.remove(3);
-        assert_eq!(obj, Some(40));
-    }
-
-    #[test]
-    fn test_length_is_0() {
-        let list: List<i32> = List::new();
-        assert_eq!(list.len(), 0);
+    fn into_iter(self) -> Self::IntoIter {
+        ListIter { current: Some(self) }
     }
 }
